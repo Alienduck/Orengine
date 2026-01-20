3672163ca115ce6dc4334f4e4dc5f64a0db6eb08
Owen Neuillyr <jacquonneries@gmail.com>
2026-01-20 15:13:35 +0100

    handle mouse MBR and apply cursor logic

--- examples/wgpu.rs
+++ examples/wgpu.rs
@@ -13,6 +13,7 @@ fn main() {
 
     // We pass the .obj file here! No more hardcoding in the engine.
     let mut state = pollster::block_on(State::new(window.clone(), "pizza.obj"));
+    let mut right_mouse_pressed = false;
 
     event_loop
         .run(move |event, target| match event {
@@ -20,6 +21,29 @@ fn main() {
                 ref event,
                 window_id,
             } if window_id == state.window.id() => match event {
+                WindowEvent::MouseInput {
+                    state: ElementState::Pressed,
+                    button: MouseButton::Right,
+                    ..
+                } => {
+                    right_mouse_pressed = true;
+                    // https://github.com/rust-windowing/winit/issues/1677
+                    let _ = state
+                        .window
+                        .set_cursor_grab(winit::window::CursorGrabMode::Confined);
+                    state.window.set_cursor_visible(false);
+                }
+                WindowEvent::MouseInput {
+                    state: ElementState::Released,
+                    button: MouseButton::Right,
+                    ..
+                } => {
+                    right_mouse_pressed = false;
+                    let _ = state
+                        .window
+                        .set_cursor_grab(winit::window::CursorGrabMode::None);
+                    state.window.set_cursor_visible(true);
+                }
                 WindowEvent::CloseRequested => target.exit(),
                 WindowEvent::Resized(physical_size) => state.resize(*physical_size),
                 WindowEvent::RedrawRequested => {
@@ -34,6 +58,14 @@ fn main() {
                 ref event if state.input(event) => {}
                 _ => {}
             },
+            Event::DeviceEvent {
+                event: DeviceEvent::MouseMotion { delta },
+                ..
+            } => {
+                if right_mouse_pressed {
+                    state.handle_mouse_motion(delta);
+                }
+            }
             Event::AboutToWait => state.window.request_redraw(),
             _ => {}
         })
